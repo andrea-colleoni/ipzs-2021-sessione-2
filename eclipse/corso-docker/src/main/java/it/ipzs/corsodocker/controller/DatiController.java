4e2c916187d4e566684c50ae5887b76ad4df9f08
Andrea Colleoni <andrea@colleoni.info>
2021-08-26 17:07:29 +0200

    esercizio del pomeriggio

--- eclipse/corso-docker/src/main/java/it/ipzs/corsodocker/controller/DatiController.java
+++ eclipse/corso-docker/src/main/java/it/ipzs/corsodocker/controller/DatiController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.ipzs.corsodocker.model.Persona;
@@ -26,4 +27,14 @@ public class DatiController {
 		lista.add(p);
 		return lista;
 	}
+	
+	@GetMapping("/persone/{id}")
+	public Persona personaPerId(@PathVariable("id") String id) {
+		System.out.println(id);
+		var p = new Persona();
+		p.setNome("Mario");
+		p.setCognome("Rossi");
+		p.setEmail("mario@rossi.com");
+		return p;
+	}	
 }
---
+++ lezioni/esercizio-02.md
@@ -0,0 +1,7 @@
+# Eseritazione guidata
+
+## Realizzare un'infrasttura Docker che esponga un'API di un servizio REST fatto con Spring Boot
+
+- Preparare l'API REST in Spring Boot
+- Definire il file compose con:
+  - Un reverse proxy
