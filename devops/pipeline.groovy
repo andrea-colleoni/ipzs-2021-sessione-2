a8087312fdc2d7458d3f8a5d465cae88980501a3
Andrea Colleoni <andrea@colleoni.info>
2021-09-02 10:55:39 +0200

    prima pipeline (mattina gg2)

---
+++ devops/pipeline.groovy
@@ -0,0 +1,43 @@
+pipeline {
+    agent any
+
+    tools {
+      jdk 'Jdk-11'
+      maven 'Maven 3.8.2'
+    }
+
+    stages {
+        stage('Inizio') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+        stage('Checkout') {
+            steps {
+                git branch: 'main', 
+                    url: 'https://github.com/andrea-colleoni/ipzs-2021-sessione-2.git'
+            }
+        }
+        stage('Pulizia') {
+            steps {
+                dir('eclipse/corso-docker') {
+                    sh 'mvn clean'
+                }
+            }
+        }
+        stage('Compilazione') {
+            steps {
+                dir('eclipse/corso-docker') {
+                    sh 'mvn compile'
+                }
+            }
+        }
+        stage('Test & Package') {
+            steps {
+                dir('eclipse/corso-docker') {
+                    sh 'mvn package'
+                }
+            }
+        }           
+    }
+}
---
+++ lezioni/devops-02.md
@@ -0,0 +1,8 @@
+# DevOps - Giorno 2 (02/09/2021)
+
+## Argomenti
+- Pipeline in Jenkins: definizione di un job con linguaggio dichiarativo
+- Build di progetti Angular
+- Deployment
+  - Con strumenti classici
+  - Con Docker
